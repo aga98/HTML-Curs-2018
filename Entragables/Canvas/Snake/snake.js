ba8925c0e6d07442a4fcfa9a3e772cfe571456b3
aga98 <aga98@users.noreply.github.com>
2018-07-09 21:40:19 +0200

    DMW

--- Entragables/Canvas/Snake/snake.js
+++ Entragables/Canvas/Snake/snake.js
@@ -4,15 +4,26 @@ var size = 20 ;
 var WIDTH = 500;
 var HEIGHT = 500;
 var ctx;
+var newx;
+var newy;
+var i;
 
 var parts = 1;
 var iid;
 var dir = {u:0,d:0,l:0,r:0};
 
+window.onload = function(){
+	intialize();
+	window.onkeydown = function() {
+		key(new_event);
+	}
+}
+
 function intialize(){
 	ctx = document.getElementById('container').getContext("2d");
 	updateFruit();
 	updateSnake();
+	iid = setInterval(update,50);
 }
 
 function updateFruit(){
@@ -22,8 +33,16 @@ function updateFruit(){
 	updateSnake();
 }
 
-function updateSnake(){
-	draw(snakePos.x,snakePos.y,size,"black");
+function updateSnake(){ 
+	for(i = 0; i < parts; i++) {
+		newx = snakePos[i].x;
+		newy = snakePos[i].y;
+		if(dir.u == 1) newy = snakePos[i].y - size;
+		else if(dir.d == 1) newy = snakePos[i].y + size;
+		else if(dir.l == 1) newx = snakePos[i].x - size;
+		else if(dir.r == 1) newx = snakePos[i].x + size;
+		draw(newx,newy,size,"black");
+	}
 }
 
 function draw(x,y,size,color){
@@ -33,6 +52,31 @@ function draw(x,y,size,color){
 	ctx.fill();
 }
 
-window.onload = function(){
-	intialize();
+
+function key(pressed){
+	var x = pressed.keyCode;
+	if(x == 37) { //left
+		dir = {u:0,d:0,l:1,r:0};
+	}
+	else if(x == 38) { //up
+		dir = {u:1,d:0,l:0,r:0};
+	}
+	else if(x == 39) { //right
+		dir = {u:0,d:0,l:0,r:1};
+	}
+	else if(x == 40) { //down
+		dir = {u:0,d:1,l:0,r:0};
+	}
+}
+
+function update() {
+	ctx.clearRect(0,0,WIDTH,HEIGHT);
+	if(snakePos[0].x == fruitPos.x && snakePos[0].y == fruitPos.y) {
+		updateFruit();
+		++parts;
+	}
+	else {
+		draw(fruitPos.x,fruitPos.y,size,"red");
+		updateSnake();
+	}
 }
