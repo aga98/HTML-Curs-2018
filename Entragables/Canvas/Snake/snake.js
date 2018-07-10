9e64ba57dbd9c1e17304bc345a45cc042afe9f40
aga98 <aga98@users.noreply.github.com>
2018-07-10 12:51:15 +0200

    snake

--- Entragables/Canvas/Snake/snake.js
+++ Entragables/Canvas/Snake/snake.js
@@ -1,4 +1,4 @@
-var snakePos = {x:0, y:0};
+var snakePos = [{x:0, y:0}];
 var fruitPos = {x:0, y:0};
 var size = 20 ;
 var WIDTH = 500;
@@ -7,23 +7,24 @@ var ctx;
 var newx;
 var newy;
 var i;
-
 var parts = 1;
 var iid;
-var dir = {u:0,d:0,l:0,r:0};
+var dir = {u:0,d:0,l:0,r:1};
+var newpart = false;
+var xx;
+var yy;
 
 window.onload = function(){
 	intialize();
-	window.onkeydown = function() {
-		key(new_event);
-	}
+	window.onkeydown = function(){key(event)};
+	
 }
 
 function intialize(){
 	ctx = document.getElementById('container').getContext("2d");
 	updateFruit();
 	updateSnake();
-	iid = setInterval(update,50);
+	iid = setInterval(update,150);
 }
 
 function updateFruit(){
@@ -34,15 +35,42 @@ function updateFruit(){
 }
 
 function updateSnake(){ 
-	for(i = 0; i < parts; i++) {
-		newx = snakePos[i].x;
-		newy = snakePos[i].y;
-		if(dir.u == 1) newy = snakePos[i].y - size;
-		else if(dir.d == 1) newy = snakePos[i].y + size;
-		else if(dir.l == 1) newx = snakePos[i].x - size;
-		else if(dir.r == 1) newx = snakePos[i].x + size;
-		draw(newx,newy,size,"black");
+	if(newpart == true){
+		newpart = false;
+		for(i = 1; i < parts-1; i++) {
+			newx = snakePos[i-1].x;
+			newy = snakePos[i-1].y;
+			draw(newx,newy,size,"black");
+			snakePos[i].x = newx;
+			snakePos[i].y = newy;
+			if(i == parts-1){
+				newpart = false;
+				snakePos.push({x:xx, y:yy});
+			}	
+		}		
 	}
+	else{
+		for(i = 1; i < parts; i++) {
+			newx = snakePos[i-1].x;
+			newy = snakePos[i-1].y;
+			draw(newx,newy,size,"black");
+			snakePos[i].x = newx;
+			snakePos[i].y = newy;
+			if(newpart == true && i == parts-1){
+				newpart = false;
+				snakePos.push({x:xx, y:yy});
+			}			
+		}
+	}
+	newx = snakePos[0].x;
+	newy = snakePos[0].y;
+	if(dir.u == 1) newy = snakePos[0].y - size;
+	else if(dir.d == 1) newy = snakePos[0].y + size;
+	else if(dir.l == 1) newx = snakePos[0].x - size;
+	else if(dir.r == 1) newx = snakePos[0].x + size;
+	snakePos[0].x = newx;
+	snakePos[0].y = newy;
+	draw(newx,newy,size,"black");
 }
 
 function draw(x,y,size,color){
@@ -53,8 +81,8 @@ function draw(x,y,size,color){
 }
 
 
-function key(pressed){
-	var x = pressed.keyCode;
+function key(event){
+	var x = event.keyCode;
 	if(x == 37) { //left
 		dir = {u:0,d:0,l:1,r:0};
 	}
@@ -72,8 +100,11 @@ function key(pressed){
 function update() {
 	ctx.clearRect(0,0,WIDTH,HEIGHT);
 	if(snakePos[0].x == fruitPos.x && snakePos[0].y == fruitPos.y) {
-		updateFruit();
 		++parts;
+		newpart = true;
+		xx = snakePos[parts-2].x;
+		yy = snakePos[parts-2].y;
+		updateFruit();
 	}
 	else {
 		draw(fruitPos.x,fruitPos.y,size,"red");
