ef30e3f3c7ca735a65260afa61dd752150707599
aga98 <aga98@users.noreply.github.com>
2018-07-11 16:39:26 +0200

    DMW

--- Project/js/main.js
+++ Project/js/main.js
@@ -1,19 +1,19 @@
 var iid;
 var vid = "canyon";
 
-$(window).("load", function(){
-	iid = setInterval(switchVideo,1000);
+$(document).ready(function(){
+	iid = setInterval(switchVideo,15000);
 });
 
 function switchVideo(){
 	if(vid == "canyon"){
 		vid = "niagara";
-			$("#canyon").css(display,"none");
-			$("#niagara").css(display,"initial");
+			$("#canyon").css("display","none");
+			$("#niagara").css("display","initial");
 	}
 	else{
 		vid = "canyon";
-			$("#niagara").css(display,"none");
-			$("#canyon").css(display,"initial");
+			$("#niagara").css("display","none");
+			$("#canyon").css("display","initial");
 	}
 }
