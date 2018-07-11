6ad611425f93a20265d6d524b409f75042cf9a8d
aga98 <aga98@users.noreply.github.com>
2018-07-11 19:59:59 +0200

    DMW

--- Project/js/main.js
+++ Project/js/main.js
@@ -3,6 +3,15 @@ var vid = "canyon";
 
 $(document).ready(function(){
 	iid = setInterval(switchVideo,15000);
+
+	$("#night").on("change",function(){
+    	if($(this).is(':checked')) {
+    	    darken();
+    	} 
+	    else {
+	        light();
+	    }
+	});
 });
 
 function switchVideo(){
@@ -17,3 +26,17 @@ function switchVideo(){
 			$("#canyon").css("display","initial");
 	}
 }
+
+
+function darken(){
+	$(document.body).css("background-color","#171a21");
+	$(".header-country").css("color","white");
+	$("section div div").css("background-color","#878787");
+}
+
+function light(){
+	$(document.body).css("background-color","white");
+	$(".header-country").css("color","#545454");
+}
+
+
