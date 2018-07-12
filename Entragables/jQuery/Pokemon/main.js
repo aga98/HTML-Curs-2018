a22c5a02ea77e9867c4af33402ed2a84389a3675
aga98 <aga98@users.noreply.github.com>
2018-07-12 19:57:47 +0200

    DMW

--- Entragables/jQuery/Pokemon/main.js
+++ Entragables/jQuery/Pokemon/main.js
@@ -1,9 +1,8 @@
 $().ready(function(){
 	var id = 1;
-	$.get("https:// 40.118.8.76/pokemons/" + id,
+	$.get("https://40.118.8.76/pokemons/" + id,
 		function(data){
-			$("#name").text(data.name);
-			$("#eye").html(data.films[1]);
+			$("#code").text(data.code);
 		}, "json")
 	.always(function(){
 		alert($("#name").text());
--- Project/js/main.js
+++ Project/js/main.js
@@ -3,6 +3,17 @@ var vid = "canyon";
 
 $(document).ready(function(){
 	iid = setInterval(switchVideo,15000);
+	//automatic dark mode
+	var date = new Date();
+	var hours  = date.getHours();
+	if(hours > 19 || hours < 8){ //dark
+		$("#night").prop("checked", true);
+		darken();
+	}
+	else{ //light
+		$("#night").prop("checked", false);
+		light();
+	}
 
 	$("#night").on("change",function(){
     	if($(this).is(':checked')) {
@@ -10,7 +21,13 @@ $(document).ready(function(){
     	} 
 	    else {
 	        light();
-	    }
+	    }	    
+	});
+	$(".photo-container").on("mouseover", function(){
+		$(this).find("div").css("background-color", "#7D7D7D");	
+	});
+	$(".photo-container").on("mouseout", function(){
+		$(this).find("div").css("background-color", "#404040");	
 	});
 });
 
@@ -31,12 +48,15 @@ function switchVideo(){
 function darken(){
 	$(document.body).css("background-color","#171a21");
 	$(".header-country").css("color","white");
-	$("section div div").css("background-color","#878787");
+	$("section div div").css("background-color","#404040");
+	$("footer").css("background-color","#404040");
 }
 
 function light(){
 	$(document.body).css("background-color","white");
 	$(".header-country").css("color","#545454");
+	$("section div div").css("background-color","#474747");
+	$("footer").css("background-color","#171a21");
 }
 
 
